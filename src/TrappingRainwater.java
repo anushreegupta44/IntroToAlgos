af9cff2d41cef4597d074eb34c636ac97f314b38
Anushree Gupta <ganu@thoughtworks.com>
2019-04-07 18:39:39 +0530

    [Anushree] Renaming instance variables

--- src/TrappingRainwater.java
+++ src/TrappingRainwater.java
@@ -1,8 +1,8 @@
 public class TrappingRainwater {
-  int[] list;
+  private int[] array;
 
-  public TrappingRainwater(int[] list) {
-    this.list = list;
+  public TrappingRainwater(int[] array) {
+    this.array = array;
   }
 
   public static void main(String[] args) {
@@ -18,20 +18,20 @@ public class TrappingRainwater {
     int leftMax = 0;
     int rightMax = 0;
     int lo = 0;
-    int hi = list.length - 1;
+    int hi = array.length - 1;
     while (lo <= hi) {
-      if (list[lo] < list[hi]) {
-        if (list[lo] > leftMax) {
-          leftMax = list[lo];
+      if (array[lo] < array[hi]) {
+        if (array[lo] > leftMax) {
+          leftMax = array[lo];
         } else {
-          water += leftMax - list[lo];
+          water += leftMax - array[lo];
         }
         lo++;
       } else {
-        if (list[hi] > rightMax) {
-          rightMax = list[hi];
+        if (array[hi] > rightMax) {
+          rightMax = array[hi];
         } else {
-          water += rightMax - list[hi];
+          water += rightMax - array[hi];
         }
         hi--;
       }
@@ -40,30 +40,30 @@ public class TrappingRainwater {
   }
 
   private int maximumWaterHeld() {
-    int[] leftMax = new int[list.length];
-    int[] rightMax = new int[list.length];
-    leftMax[0] = list[0];
-    for (int i = 1; i < list.length; i++) {
-      if (list[i] > leftMax[i - 1]) {
-        leftMax[i] = list[i];
+    int[] leftMax = new int[array.length];
+    int[] rightMax = new int[array.length];
+    leftMax[0] = array[0];
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] > leftMax[i - 1]) {
+        leftMax[i] = array[i];
       } else {
         leftMax[i] = leftMax[i - 1];
       }
     }
-    rightMax[list.length - 1] = list[list.length - 1];
-    for (int i = list.length - 2; i >= 0; i--) {
-      if (list[i] > rightMax[i + 1]) {
-        rightMax[i] = list[i];
+    rightMax[array.length - 1] = array[array.length - 1];
+    for (int i = array.length - 2; i >= 0; i--) {
+      if (array[i] > rightMax[i + 1]) {
+        rightMax[i] = array[i];
       } else {
         rightMax[i] = rightMax[i + 1];
       }
     }
     int maxWater = 0;
-    for (int i = 0; i < list.length; i++) {
+    for (int i = 0; i < array.length; i++) {
       if (leftMax[i] > rightMax[i]) {
-        maxWater += rightMax[i] - list[i];
+        maxWater += rightMax[i] - array[i];
       } else {
-        maxWater += leftMax[i] - list[i];
+        maxWater += leftMax[i] - array[i];
       }
     }
     return maxWater;
