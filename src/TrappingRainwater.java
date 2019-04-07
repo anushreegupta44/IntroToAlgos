3d3e38715bb5bbf7efd935b7ab3b459cef58cc9a
Anushree Gupta <ganu@thoughtworks.com>
2019-04-07 10:08:14 +0530

    [Anushree] Trapping rainwater - optimised space

--- src/TrappingRainwater.java
+++ src/TrappingRainwater.java
@@ -7,8 +7,36 @@ public class TrappingRainwater {
 
   public static void main(String[] args) {
     TrappingRainwater trappingRainwater = new TrappingRainwater(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
-    System.out.print(trappingRainwater.maximumWaterHeld());
+    System.out.println(trappingRainwater.maximumWaterHeld());
+    System.out.print(trappingRainwater.trap());
 
+  }
+
+  //optimised space complexity, no extra arrays
+  private int trap() {
+    int water = 0;
+    int leftMax = 0;
+    int rightMax = 0;
+    int lo = 0;
+    int hi = list.length - 1;
+    while (lo <= hi) {
+      if (list[lo] < list[hi]) {
+        if (list[lo] > leftMax) {
+          leftMax = list[lo];
+        } else {
+          water += leftMax - list[lo];
+        }
+        lo++;
+      } else {
+        if (list[hi] > rightMax) {
+          rightMax = list[hi];
+        } else {
+          water += rightMax - list[hi];
+        }
+        hi--;
+      }
+    }
+    return water;
   }
 
   private int maximumWaterHeld() {
